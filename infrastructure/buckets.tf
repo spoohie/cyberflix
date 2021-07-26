720348844f40bb3f969145fc1077dd446f05b45b
spoohie <kplatkowski@outlook.com>
2021-07-26 12:35:58 +0200

    Change bucket names

--- infrastructure/buckets.tf
+++ infrastructure/buckets.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_bucket" "input" {
-  bucket = "kplatkow-workshop-input"
+  bucket = "cyberflix-input"
   force_destroy = true
   acl    = "private"
 
   tags = var.tags
 }
 
-resource "aws_s3_bucket" "destination" {
-  bucket = "kplatkow-workshop-destination"
+resource "aws_s3_bucket" "output" {
+  bucket = "cyberflix-output"
   force_destroy = true
   acl    = "private"
 
